c82c06085cea4cc5616c14dd477fb41ceb15ac9a
Nick Tzaperas <nick@nijotz.com>
2019-11-13 10:35:07 -0800

    Better comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /**
- * Allows users to `require('@newrelic/logform')` directly in their app. If
+ * Allows users to `require('@newrelic/winston-enricher')` directly in their app. If
  * they for some reason choose to explicitly use an older version of our
  * instrumentation then the supportability metrics for custom instrumentation
  * will trigger.
