e8c22479108a53c2436ee14b165c49b19a729671
Diana Thayer <dthayer@newrelic.com>
2022-05-05 12:26:24 -0700

    convert tests to unit tests

--- packages/winston-log-enricher/tests/unit/auto.tap.js
+++ packages/winston-log-enricher/tests/unit/auto.tap.js
@@ -12,45 +12,35 @@ const API = require('newrelic/api')
 
 utils(tap)
 
-tap.test('auto-instrumentation of winston', (t) => {
+tap.test('auto-instrumentation of winston', { bail: true }, (t) => {
   t.autoend()
 
   let helper
   let winston
   let api
 
+  t.afterEach(() => {
+    Object.keys(require.cache).forEach((key) => {
+      if (/winston/.test(key)) {
+        delete require.cache[key]
+      }
+    })
+  })
+
   t.test('when auto-instrumentation occurs', (t) => {
     t.autoend()
 
     t.beforeEach(() => {
       helper = utils.TestAgent.makeInstrumented()
-      helper.agent.config.application_logging = {
-        enabled: true,
-        metrics: {
-          enabled: true
-        },
-        forwarding: {
-          enabled: true
-        }
-      }
       winston = require('winston')
       api = new API(helper.agent)
+      winston.createLogger.__NR_original = true
     })
 
     t.afterEach(() => {
       helper.unload()
       winston = null
       api = null
-      Object.keys(require.cache).forEach((key) => {
-        if (/winston/.test(key)) {
-          delete require.cache[key]
-        }
-      })
-    })
-
-    t.test('should have instrumented winston automatically', (t) => {
-      t.autoend()
-      t.ok(api.shim.isWrapped(winston.createLogger))
     })
 
     t.test(
@@ -68,15 +58,6 @@ tap.test('auto-instrumentation of winston', (t) => {
 
     t.beforeEach(() => {
       helper = utils.TestAgent.makeInstrumented()
-      helper.agent.config.application_logging = {
-        enabled: false,
-        metrics: {
-          enabled: true
-        },
-        forwarding: {
-          enabled: true
-        }
-      }
       winston = require('winston')
       api = new API(helper.agent)
     })
