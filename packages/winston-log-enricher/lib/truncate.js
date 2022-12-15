8a271d79b2937ebbd30a8403cfc4879dcd84d3c0
Jeff Mealo <jmealo@shutterstock.com>
2022-12-14 19:09:04 -0500

    Fix: Truncate shouldn't throw on invalid input

--- packages/winston-log-enricher/lib/truncate.js
+++ packages/winston-log-enricher/lib/truncate.js
@@ -8,7 +8,7 @@
 const MAX_LENGTH = 1021
 const OUTPUT_LENGTH = 1024
 const truncate = (str) => {
-  if (str.length > OUTPUT_LENGTH) {
+  if (typeof str === 'string' && str.length > OUTPUT_LENGTH) {
     return str.substring(0, MAX_LENGTH) + '...'
   }
 
