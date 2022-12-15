264346b3a531be63b00260c37335bc0930bf0330
Jeff Mealo <jmealo@shutterstock.com>
2022-12-15 10:39:44 -0500

    Fix: Trigger CI by non-NR employee :)

--- packages/pino-log-enricher/lib/truncate.js
+++ packages/pino-log-enricher/lib/truncate.js
@@ -5,8 +5,9 @@
 
 'use strict'
 
-const MAX_LENGTH = 1021
 const OUTPUT_LENGTH = 1024
+const MAX_LENGTH = 1021
+
 const truncate = (str) => {
   if (typeof str === 'string' && str.length > OUTPUT_LENGTH) {
     return str.substring(0, MAX_LENGTH) + '...'
